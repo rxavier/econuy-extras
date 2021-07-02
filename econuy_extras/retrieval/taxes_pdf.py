2464a7d0ebbdf7508ba9923d91bb0f3a5c4eb491
Rafael Xavier <rxaviermontero@gmail.com>
2021-07-02 02:52:45 -0300

    Getting closer

--- econuy_extras/retrieval/taxes_pdf.py
+++ econuy_extras/retrieval/taxes_pdf.py
@@ -73,7 +73,7 @@ def get_taxes_from_pdf(start_year: int = 2020) -> pd.DataFrame:
                     'Recaudación Total de la DGI']
                 data.append(table)
     output = pd.concat(data)
-    output.to_csv("taxes.csv")
+    output.to_csv("econuy_extras/retrieval/taxes.csv")
 
     return output
 
